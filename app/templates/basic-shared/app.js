806c5f6812e2e7c1f31fe26d46104a5b8e50a201
jsdevel <js.developer.undefined@gmail.com>
2015-08-18 11:00:52 -0700

    Initial Commit

--- app/templates/basic-shared/app.js
+++ app/templates/basic-shared/app.js
@@ -1,33 +1,16 @@
-<% if(options.coffee){ %>require('coffee-script/register');<% } %>
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');<% if(options.viewEngine == 'swig'){ %>
-var swig = require('swig');<% } %><% if(options.viewEngine == 'handlebars'){ %>
-var exphbs  = require('express-handlebars');<% } %>
-
+var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var users = require('./routes/user');
 
 var app = express();
 
 var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
-
-// view engine setup
-<% if(options.viewEngine == 'swig'){ %>app.engine('swig', swig.renderFile)
-app.set('view cache', false);
-swig.setDefaults({ cache: false });<% } %><% if(options.viewEngine == 'handlebars'){ %>
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  partialsDir: ['views/partials/']
-}));<% } %><% if(options.viewEngine != 'marko'){ %>
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', '<%= options.viewEngine %>');<% } %>
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
@@ -52,38 +35,26 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-<% if(options.viewEngine == 'marko'){ %>
-  var errorTemplate = require('marko').load(require.resolve('./views/error.marko'));<% } %>
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);<% if(options.viewEngine == 'marko'){ %>
-        errorTemplate.render({
-          message: err.message,
-          error: err,
-          title: 'error'
-        }, res);<% } else { %>
+        res.status(err.status || 500);
         res.render('error', {
             message: err.message,
             error: err,
             title: 'error'
-        });<% } %>
+        });
     });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);<% if(options.viewEngine == 'marko'){ %>
-    errorTemplate.render({
-      message: err.message,
-      error: err,
-      title: 'error'
-    }, res);<% } else { %>
+    res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {},
         title: 'error'
-    });<% } %>
+    });
 });
 
 
--- app/templates/basic/routes/index.js
+++ app/templates/basic/routes/index.js
@@ -1,16 +1,6 @@
 var express = require('express');
-var router = express.Router();<% if(options.viewEngine == 'marko'){ %>
-var marko = require('marko');<% } %>
+var router = express.Router();
 
-/* GET home page. */
-<% if(options.viewEngine == 'marko'){ %>
-var indexTemplate = marko.load(require.resolve('../views/index.marko'));<% } %>
-router.get('/', function(req, res) {<% if(options.viewEngine == 'marko'){ %>
-  indexTemplate.render({
-    $global: {locals: req.app.locals},
-    title: 'Express'
-  }, res);<% } else { %>
-  res.render('index', { title: 'Express' });<% } %>
-});
+// TODO add routes here
 
 module.exports = router;
